4aa82df6c1f9716b7ce980abd459e37bf88318dc
Fcant <fcscanf@outlook.com>
2024-03-31 15:39:20 +0800

    :art: format code

--- httputils/querystring.go
+++ httputils/querystring.go
@@ -6,15 +6,14 @@ import (
 	"reflect"
 )
 
-func QueryStringDecoder4Request(r *http.Request, val *any) error {
+func QueryStringDecoder4Request(r *http.Request, val any) error {
 	t := reflect.TypeOf(val)
 	if t.Kind() != reflect.Struct {
 		return fmt.Errorf("unsupported argument type")
 	}
 	v := reflect.ValueOf(val)
 	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
-		tag := field.Tag.Get("url")
+		tag := t.Field(i).Tag.Get("url")
 		if tag != "" && v.Field(i).Kind() == reflect.String {
 			v.Field(i).SetString(r.FormValue(tag))
 		}
