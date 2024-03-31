08533101f5651237f948771b8f8da5bb5520df72
Fcant <fcscanf@outlook.com>
2024-03-31 16:11:00 +0800

    :bug: fix bug

--- httputils/querystring.go
+++ httputils/querystring.go
@@ -9,7 +9,7 @@ import (
 func QueryStringDecoder4Request(r *http.Request, val any) error {
 	t := reflect.TypeOf(val)
 	v := reflect.ValueOf(val).Elem()
-	if t.Kind() != reflect.Pointer || v.IsNil() {
+	if t.Kind() != reflect.Pointer {
 		return fmt.Errorf("unsupported argument type")
 	}
 	for i := 0; i < v.NumField(); i++ {
