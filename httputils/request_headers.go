0230c894c494b5bac36333d0fe15ee8f12335e6c
Fcant <fcscanf@outlook.com>
2024-09-11 16:43:15 +0800

    :sparkles: add general request header

--- httputils/request_headers.go
+++ httputils/request_headers.go
@@ -22,7 +22,32 @@ func (header RequestHeader) UserAgent(userAgent string) RequestHeader {
 	return header
 }
 
+func (header RequestHeader) UserAgentWindows() RequestHeader {
+	header["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
+	return header
+}
+
+func (header RequestHeader) UserAgentAndroid() RequestHeader {
+	header["User-Agent"] = "Mozilla/5.0 (Linux; Android 12; ABR-AL80 Build/HUAWEIABR-AL80; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 "
+	return header
+}
+
 func (header RequestHeader) ContentType(contentType string) RequestHeader {
 	header["Content-Type"] = contentType
+	return header
+}
+
+func (header RequestHeader) ContentTypeApplicationJSON() RequestHeader {
+	header["Content-Type"] = "application/json;charset=UTF-8"
+	return header
+}
+
+func (header RequestHeader) ContentTypeApplicationXML() RequestHeader {
+	header["Content-Type"] = "application/xml;charset=UTF-8"
+	return header
+}
+
+func (header RequestHeader) ContentTypeTextXML() RequestHeader {
+	header["Content-Type"] = "text/xml;charset=UTF-8"
 	return header
 }
