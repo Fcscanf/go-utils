fa2f69eea8581560328890fe199a439436f25bdc
Fcant <fcscanf@outlook.com>
2024-10-13 14:51:03 +0800

    :necktie: Check whether it is enabled when obtaining the network interface IP

--- netutils/net_utils.go
+++ netutils/net_utils.go
@@ -4,7 +4,7 @@ import (
 	"net"
 )
 
-// LocalIPv4s Get the IPv4 address on the local network interface
+// LocalIPv4s Get the IPv4 address on a locally enabled network interface
 func LocalIPv4s() ([]string, error) {
 	var ips []string
 	// 获取所有网络接口
@@ -16,6 +16,10 @@ func LocalIPv4s() ([]string, error) {
 	// 遍历每个接口，获取相关的IP地址
 	for _, iface := range interfaces {
 		//fmt.Printf("网络接口: %s\n", iface.Name)
+		// 检查网络接口是否启用，未启用则跳过
+		if iface.Flags&net.FlagUp == 0 {
+			continue
+		}
 		// 获取接口的地址列表
 		addrs, err := iface.Addrs()
 		if err != nil {
